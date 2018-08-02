87901816ed51b7a7c52a734d3e779f4f99d0e230
manan <mananbruce@qq.com>
2018-08-02 22:38:25 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: manan
+  Date: 2018/8/2
+  Time: 21:36
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>hello</title>
+  </head>
+  <body>
+  <h1 style="color: red;">可以</h1>
+  </body>
+</html>
---
+++ src/com/manan/hello.java
@@ -0,0 +1,20 @@
+package com.manan;
+
+public class hello {
+    public static void main(String[] args) {//spvm
+        System.out.println();//sout
+        System.out.println("args = [" + args + "]");//soutp
+        System.out.println("hello.main");//soutm
+        System.out.println("args = " + args);//soutv
+
+        int a[] = {1,2};
+
+
+        for (int i : a) {
+            System.out.println(i);
+        }
+        for (int i = 0; i <1 ; i++) {
+            
+        }
+    }
+}
